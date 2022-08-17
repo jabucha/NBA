8812c73513d9bf49e3eb5ba141878c6a36cb0df5
Jeffery Buchanan <93730396+jabucha@users.noreply.github.com>
2022-08-17 00:12:15 -0700

    Work in Progress

---
+++ Traditional Stats Playoff and Regular Season 1996-2022.py
@@ -0,0 +1,69 @@
+"""
+NBA STATS DATABASE
+"""
+# This code was made available from the original author Ryan Davis https://github.com/rd11490 found under the NBA_Tutorials and finding_endpoints.
+# Minor changes were made in order to accommodate my requests and an updated Chrome browser due to the code being outdated.
+
+import pandas as pd
+import requests
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+header_data  = {
+    'Connection': 'keep-alive',
+    'Accept': 'application/json, text/plain, */*',
+    'x-nba-stats-token': 'true',
+    'User-Agent': 'Chrome/104.0.5112.81',                       # current Chrome version, other options are available
+    'x-nba-stats-origin': 'stats',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-Mode': 'cors',
+    'Referer': 'https://stats.nba.com/',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+}
+
+
+
+
+# Extract json
+def extract_data(url):
+    r = requests.get(url, headers=header_data)                  # Call the GET endpoint
+    resp = r.json()                                             # Convert the response to a json object
+    results = resp['resultSets'][0]                             # take the first item in the resultsSet (This can be determined by inspection of the json response)
+    headers = results['headers']                                # take the headers of the response (our column names)
+    rows = results['rowSet']                                    # take the rows of our response
+    frame = pd.DataFrame(rows)                                  # convert the rows to a dataframe
+    frame.columns = headers                                     # set our column names using the  extracted headers
+    return frame
+
+
+
+# endpoints
+# must update url and season year for every data extraction
+
+def regular_player_stats_url(season):
+    return "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2021-22&SeasonSegment=&SeasonType=Regular+Season&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=".format(
+        season)
+
+season = "2021-22"
+
+frame = extract_data(regular_player_stats_url(season))
+print(frame)
+
+frame.to_csv("regular_season_stats_nba_player_data_{0}.csv".format(season), index=False)
+
+# Using this endpoint I am able to gather all the statistics from the playoffs and regular seasons up to the 1996 season.
+
+# By changing the seasontype from regular+season to playoffs and the season for each year, you can perform the same order of operations to get all playoff stats up to the 1996 season.
+
+def playoffs_player_stats_url(season):
+    return "https://stats.nba.com/stats/leaguedashplayerstats?College=&Conference=&Country=&DateFrom=&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0&LeagueID=00&Location=&MeasureType=Base&Month=0&OpponentTeamID=0&Outcome=&PORound=0&PaceAdjust=N&PerMode=PerGame&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N&Season=2021-22&SeasonSegment=&SeasonType=Playoffs&ShotClockRange=&StarterBench=&TeamID=0&TwoWay=0&VsConference=&VsDivision=&Weight=".format(
+        season)
+
+season = "2021-22"
+
+frame = extract_data(playoffs_player_stats_url(season))
+print(frame)
+
+frame.to_csv("playoffs_stats_nba_player_data_{0}.csv".format(season), index=False)
